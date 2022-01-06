5a00143a878b06e2dc876376d08c5f70e8fa2a73
Luiz Henrique <luizhjramos@outlook.com>
2022-01-06 11:32:07 -0300

    feat: Function Declaration and Function expression concept review

--- functions/function_1.js
+++ functions/function_1.js
@@ -1,1 +1,11 @@
-console.log("test")
+// Function Declaration
+function goodMorning() {
+    console.log("Good Morning")
+}
+goodMorning()
+
+// Function expression
+const goodAfternoon = function () {
+    console.log("Good Afternoon")
+}
+goodAfternoon()
