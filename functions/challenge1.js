4018e2de51026467b22c008b244a3bfc3b990f77
Luiz Henrique <luizhjramos@outlook.com>
2022-01-06 20:32:40 -0300

    feat: challenge2 solved

--- functions/challenge1.js
+++ functions/challenge1.js
@@ -8,3 +8,17 @@ const soma = (a) => {
 }
 
 console.log(soma(2)(5)(10))
+
+// Challenge2
+const somaChallenge2 = (a, b) => a + b
+
+function receiveValueA(a) {
+    return function(b){
+        return function(fn) {
+            return fn(a,b)
+        }
+    }
+}
+
+const result = receiveValueA(5)(1009)(somaChallenge2)
+console.log(result)
