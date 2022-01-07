1cd716d9032036269fddd5e2eb54f48823e0efcb
Luiz Henrique <luizhjramos@outlook.com>
2022-01-07 20:36:22 -0300

    feat: reading files synchronously

--- functions/callback_2.js
+++ functions/callback_2.js
@@ -3,8 +3,17 @@ const path = require("path")
 
 const way = path.join(__dirname, "data.txt")
 
-const showContent = (err, content) => {
+
+const showContent = (_, content) => {
     console.log(content.toString())
 }
 
+console.log("started")
 fs.readFile(way, {}, showContent)
+fs.readFile(way, (_, content) => console.log(content.toString()))
+console.log("end")
+
+console.log("Sync started")
+const content = fs.readFileSync(way)
+console.log(content.toString())
+console.log("Sync end")
