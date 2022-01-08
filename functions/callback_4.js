39346257d14f9f72eb8aaca4be19a6365e7e7dba
Luiz Henrique <luizhjramos@outlook.com>
2022-01-08 14:24:58 -0300

    feat: cart data in the callback_4 file

---
+++ functions/callback_4.js
@@ -0,0 +1,7 @@
+const cart = [
+    { name: "Pen",  quantity: 10, price: 7.99 },
+    { name: "printer",  quantity: 0, price: 649.50 },
+    { name: "Copybook",  quantity: 4, price: 27.10 },
+    { name: "Pencil",  quantity: 3, price: 2.50 },
+    { name: "Scissors",  quantity: 1, price: 19.20 },
+]
