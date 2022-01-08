b3ea8ddd049e58bfd3c449448bf01c15e1e276a0
Luiz Henrique <luizhjramos@outlook.com>
2022-01-07 22:40:52 -0300

    feat: cart data

--- functions/callback_3.js
+++ functions/callback_3.js
@@ -6,3 +6,11 @@ const names = ["Ana", "Bia", "Gui", "Lia", "Rafa"]
 const firstLetter = item => item[0]
 const letter = names.map(firstLetter)
 console.log(names, letter)
+
+const cart = [
+    { nome: "Pen",  quantity: 10, price: 7.99 },
+    { nome: "printer",  quantity: 0, price: 649.50 },
+    { nome: "Copybook",  quantity: 4, price: 27.10 },
+    { nome: "Pencil",  quantity: 3, price: 2.50 },
+    { nome: "Scissors",  quantity: 1, price: 19.20 },
+]
