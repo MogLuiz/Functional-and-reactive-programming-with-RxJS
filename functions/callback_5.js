9cc207b6f9455c09609212b76a29c834a6b27040
Luiz Henrique <luizhjramos@outlook.com>
2022-01-08 17:04:03 -0300

    feat: generalTotal function with Map and Reducer

--- functions/callback_5.js
+++ functions/callback_5.js
@@ -2,6 +2,15 @@ const cart = [
     { name: "Pen",  quantity: 10, price: 7.99 },
     { name: "printer",  quantity: 0, price: 649.50 },
     { name: "Copybook",  quantity: 4, price: 27.10 },
-    { name: "Pencil",  quantity: 3, price: 2.50 },
+    { name: "Pencil",  quantity: 3, price: 5.82 },
     { name: "Scissors",  quantity: 1, price: 19.20 },
 ]
+
+const totalPrice = item => item.price * item.quantity
+const sumTotal = (acc, el) => acc + el
+
+const generalTotal = cart
+                        .map(totalPrice)
+                        .reduce(sumTotal)
+
+console.log(generalTotal)
