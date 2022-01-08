1a5d49f9312c9ccceb747082792d6c811d40b128
Luiz Henrique <luizhjramos@outlook.com>
2022-01-08 17:50:18 -0300

    feat: cart data in the challenge2 file

---
+++ functions/challenge2.js
@@ -0,0 +1,7 @@
+const cart = [
+    { name: "Pen",  quantity: 10, price: 7.99, fragility: true },
+    { name: "printer",  quantity: 1, price: 649.50, fragility: true },
+    { name: "Copybook",  quantity: 4, price: 27.10, fragility: false },
+    { name: "Pencil",  quantity: 3, price: 5.82, fragility: false },
+    { name: "Scissors",  quantity: 1, price: 19.20, fragility: true },
+]
