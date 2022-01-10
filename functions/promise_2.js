51440c7ce9f702e589939b0f2fbf6466640f138a
Luiz Henrique <luizhjramos@outlook.com>
2022-01-09 23:02:57 -0300

    feat: composing calls using promises

--- functions/promise_2.js
+++ functions/promise_2.js
@@ -3,9 +3,23 @@ setTimeout(() => {
 
     setTimeout(() => {
         console.log("Running callback2...")
-        
+
         setTimeout(() => {
             console.log("Running callback3...")   
         }, 2000)   
     }, 2000)
 }, 2000)
+
+
+const awaitFor = (time = 2000) => {
+    return new Promise((resolve) => {
+        console.log("Awaiting Promise...")
+        setTimeout(() => {
+            resolve(["Luiz", "Henrique"])
+        }, time)
+    })
+}
+
+awaitFor()
+    .then(() => awaitFor())
+    .then(awaitFor)
