ba5219a8ec13cfe60018947c18089de73a9e0d9c
Luiz Henrique <luizhjramos@outlook.com>
2022-01-10 20:19:48 -0300

    feat: handling promise error

--- functions/promise_5.js
+++ functions/promise_5.js
@@ -1,7 +1,15 @@
 const workOrNot = (value, errorPossibility) => {
     return new Promise((resolve, reject) => {
+       try {
         if(Math.random() < errorPossibility) reject("an error has occurred")
             
         resolve(value)
-    })
+       } catch(e) {
+           reject(e)
+       }
+    }) 
 }
+
+workOrNot("testing...", 0.1)
+    .then(value => console.log(`Valor: ${value}`))
+    .catch(err => console.log(`Erro: ${err}`)) 
