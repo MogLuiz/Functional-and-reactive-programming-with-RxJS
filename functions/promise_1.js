2589692c720bde135a93ad1e4e5ee5a95dc3de28
Luiz Henrique <luizhjramos@outlook.com>
2022-01-09 22:41:23 -0300

    refact: modularizing promise response handling

--- functions/promise_1.js
+++ functions/promise_1.js
@@ -1,7 +1,12 @@
-let p = new Promise((keepPromise) => keepPromise(["Ana", "Bia", "Carlos", "Luiz"]))
+let promis = new Promise((keepPromise) => keepPromise(["Ana", "Bia", "Carlos", "Luiz"]))
 
-p
-    .then(res => res[0])
-    .then(first => first[0])
-    .then(letter => letter.toLowerCase())
-    .then(lowerCaseLetter => console.log(lowerCaseLetter))
+const getFirstName = (res) => res[0] 
+const getFirstLetter = (firstName => firstName[0])
+const parseToLowerCase = (letter => letter.toLowerCase())
+const printResponse = (lowerCaseLetter => console.log(lowerCaseLetter))
+
+promis
+    .then(getFirstName)
+    .then(getFirstLetter)
+    .then(parseToLowerCase)
+    .then(printResponse)
