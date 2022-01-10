cc3d5e04053c7ab792db5c90f73e42a87905652c
Luiz Henrique <luizhjramos@outlook.com>
2022-01-09 22:46:50 -0300

    refact: refact code and use console.log for reference

--- functions/promise_1.js
+++ functions/promise_1.js
@@ -1,12 +1,11 @@
-let promis = new Promise((keepPromise) => keepPromise(["Ana", "Bia", "Carlos", "Luiz"]))
+let promis = new Promise((resolve) => resolve(["Ana", "Bia", "Carlos", "Luiz"]))
 
 const getFirstName = (res) => res[0] 
 const getFirstLetter = (firstName => firstName[0])
 const parseToLowerCase = (letter => letter.toLowerCase())
-const printResponse = (lowerCaseLetter => console.log(lowerCaseLetter))
 
 promis
     .then(getFirstName)
     .then(getFirstLetter)
     .then(parseToLowerCase)
-    .then(printResponse)
+    .then(console.log)
