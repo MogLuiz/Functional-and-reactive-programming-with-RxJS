7d95ea5ce80903652212cc1665e449304cb6fce8
Luiz Henrique <luizhjramos@outlook.com>
2022-01-10 09:05:45 -0300

    feat: manipuling the promise response in the challenge 3

--- functions/challenge3.js
+++ functions/challenge3.js
@@ -6,7 +6,16 @@ const way = path.join(__dirname, "data.txt")
 const readFileWithPromise = (way) => {
     return new Promise(resolve => {
         fs.readFile(way, (_, content) => resolve(content.toString()))
+        console.log("Awaiting read file...")
     })
 }
 
-readFileWithPromise(way).then(console.log)
+const linesSplit = (file) => file.split("\n")
+const lineJoin = (text) => text.join(",")
+const textReturn = (text) => `O valor final é: ${text}`
+
+readFileWithPromise(way)
+                    .then(linesSplit)  
+                    .then(lineJoin)  
+                    .then(textReturn)  
+                    .then(console.log)
