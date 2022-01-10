920e4dba57eca6b5db03dabba0178adbec6b2193
Luiz Henrique <luizhjramos@outlook.com>
2022-01-10 08:16:53 -0300

    feat: Promise that returns a random according to the parameters received

---
+++ functions/promise_3.js
@@ -0,0 +1,10 @@
+const numberGenerateEntry = (min, max) => {
+    if(min > max) [max, min] = [min, max]
+
+    return new Promise(resolve => {
+        const random = parseInt(Math.random() * (max - min + 1)) + min
+        resolve(random)
+    })
+}
+
+numberGenerateEntry(10, 30).then(console.log)
