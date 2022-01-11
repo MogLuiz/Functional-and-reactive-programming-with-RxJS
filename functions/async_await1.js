d0a557968b596ecf6ea70cc60403bc8a090a2639
Luiz Henrique <luizhjramos@outlook.com>
2022-01-11 20:58:43 -0300

    feat: fastReturnValue function

--- functions/async_await1.js
+++ functions/async_await1.js
@@ -4,14 +4,26 @@ const awaitFor = (time = 2000) => {
     })
 }
 
-
-const running = async () => {
-    await awaitFor(1500)
-    console.log("Async/Await 1...")
-    await awaitFor(1500)
-    console.log("Async/Await 1...")
-    await awaitFor(1500)
-    console.log("Async/Await 1...")
+const returnValue = () => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(10), 5000)
+    })
 }
 
-running()
+const fastReturnValue = async () => 20
+
+const running = async () => {
+    let value = await returnValue()
+    let value2 = await fastReturnValue()
+
+    await awaitFor(1500)
+    console.log(`Async/Await ${value}...`)
+    await awaitFor(1500)
+    console.log(`Async/Await ${value + 1}...`)
+    await awaitFor(1500)
+    console.log(`Async/Await ${value + 2}...`)
+
+    return value + 3
+}
+
+running().then(console.log)
