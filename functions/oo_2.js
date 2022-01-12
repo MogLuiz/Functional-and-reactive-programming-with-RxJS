aa1ed2b6caea4cb271768a8e99dcb8e65426e32b
Luiz Henrique <luizhjramos@outlook.com>
2022-01-12 10:47:49 -0300

    feat: getters and setters

--- functions/oo_2.js
+++ functions/oo_2.js
@@ -1,19 +1,37 @@
 class Product {
-
     constructor(name, amount, discount = 0.5) {
         this.name = name
         this.amount = amount
         this.discount = discount
     }
 
-    finalAmount() {
+    get name() {
+        return `Produto: ${this._name}`
+    }
+
+    set name(newName) {
+        this._name = newName.toUpperCase()
+    }
+
+    get amount() {
+        return this._amount
+    }
+
+    set amount(newAmount) {
+        if(newAmount >= 0) this._amount = newAmount
+    }
+
+    get finalAmount() {
         return this.amount * (1 - this.discount)
     }
 }
 
 const p1 = new Product("Caneta", 8.99)
+p1.amount = -20
 console.log(p1.name)
+console.log(p1.amount)
 
-const p2 = new Product("Geladeira",998.99)
+
+const p2 = new Product("Geladeira",10000, 0.8)
 console.log(p2.amount)
-console.log(p2.finalAmount())
+console.log(p2.finalAmount)
