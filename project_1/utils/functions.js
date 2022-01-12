3a4244c647ed8c95dd3f9e7bc96a3d82c6271e39
Luiz Henrique <luizhjramos@outlook.com>
2022-01-12 19:43:49 -0300

    feat: function readFile to read the file synchronously and return a promise with the contents of each file

--- project_1/utils/functions.js
+++ project_1/utils/functions.js
@@ -4,7 +4,7 @@ const path = require("path")
 
 const readDirectory = (way) => {
     return new Promise((resolve, reject) => {
-        try {
+        try { 
             let files = fs.readdirSync(way)
             files = files.map(file => path.join(way, file))
             resolve(files)
@@ -15,6 +15,19 @@ const readDirectory = (way) => {
 }
 
 const elementsEndingWith = (array, standard) => array.filter(item => item.endsWith(standard))
+
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const content = fs.readFileSync(path, { encoding: "utf-8" })
+            resolve(content)
+        } catch(err) {
+            reject(err)
+        }
+    })
+}
+
+const readFiles = () => {}
 
 module.exports = {
     readDirectory,
