17e3d593255d253e2de561e39f221c2142af56ee
Luiz Henrique <luizhjramos@outlook.com>
2022-01-11 22:55:01 -0300

    feat: function to give discounts on products

--- functions/oo_1.js
+++ functions/oo_1.js
@@ -1,8 +1,11 @@
-function Product(name, amount) {
+function Product(name, amount, discount = 0.15) {
     this.name = name
     this.amount = amount
+    this.discount = discount
 
-    let private = 3
+    this.finalAmount = function () {
+        return this.amount * (1 - this.discount)
+    }
 }
 
 const p1 = new Product("Caneta", 8.99)
@@ -10,4 +13,5 @@ console.log(p1.name)
 
 const p2 = new Product("Geladeira",998.99)
 console.log(p2.amount)
+console.log(p2.finalAmount())
 
