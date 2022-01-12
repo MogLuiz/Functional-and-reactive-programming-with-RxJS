6f3189a797c24e3b52b028287394918b8bf96d6d
Luiz Henrique <luizhjramos@outlook.com>
2022-01-11 22:01:47 -0300

    feat: megaSenaGenerate function

--- functions/async_await2.js
+++ functions/async_await2.js
@@ -3,7 +3,19 @@ const numberGenerateEntry = (min, max, forbiddenNumbers) => {
 
     return new Promise((resolve, reject) => {
         const random = parseInt(Math.random() * (max - min + 1)) + min
-        if(forbiddenNumbers.includes(random)) reject()
+        if(forbiddenNumbers.includes(random)) reject("Numero repetido")
         resolve(random)
     })
 }
+
+const megaSenaGenerate = async (numberQuantity) => {
+    const numbers = []
+    for(let _ of Array(numberQuantity).fill()) {
+        numbers.push(await numberGenerateEntry(1, 60, numbers))
+    }
+    return numbers
+}
+
+megaSenaGenerate(8)
+                    .then(console.log)
+                    .catch(console.log)
