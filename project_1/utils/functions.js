4cc1d58cdd393349b51aefeb2f088cfad0f68f09
Luiz Henrique <luizhjramos@outlook.com>
2022-01-14 17:20:44 -0300

    feat: sortByNumericAttribute algorithm to sort my array by attribute

--- project_1/utils/functions.js
+++ project_1/utils/functions.js
@@ -88,6 +88,15 @@ const mergingWords = (words) => {
 }
 
 
+const sortByNumericAttribute = (attr, order = "asc") => {
+    return function (array) {
+        const asc = (o1, o2) => o1[attr] - o2[attr]
+        const desc = (o1, o2) => o2[attr] - o1[attr]
+        return array.sort(order === "asc" ? asc : desc)
+    }
+}
+
+
 module.exports = {
     readDirectory,
     elementsEndingWith,
