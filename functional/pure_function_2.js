35340479f0b0fbdf30632e6c15fa14f799d53101
Luiz Henrique <luizhjramos@outlook.com>
2022-01-14 23:10:40 -0300

    feat: example of a random function and it is impure because from the input values we cannot determine the output value.

---
+++ functional/pure_function_2.js
@@ -0,0 +1,10 @@
+const generateRandomNumber = (min, max) => {
+    const factor = max - min + 1
+    return parseInt(Math.random() * factor) + min
+}
+
+console.log(generateRandomNumber(5, 6))
+console.log(generateRandomNumber(5, 6))
+console.log(generateRandomNumber(5, 6))
+console.log(generateRandomNumber(5, 6))
+console.log(generateRandomNumber(5, 6))
