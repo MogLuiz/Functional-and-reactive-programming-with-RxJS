3494d8ed0c91dfa8230cb106e4c0854e3f39b5f5
Luiz Henrique <luizhjramos@outlook.com>
2022-01-14 22:37:21 -0300

    feat: using circleAreaPure as a pure function

--- functional/pure_function_1.js
+++ functional/pure_function_1.js
@@ -1,5 +1,12 @@
 const PI = 3.14
 
+// Impure function - PI is an external value
 const circleArea = (radius) => radius * radius * PI
 
-console.log(circleArea(10))
+// console.log(circleArea(10))
+
+
+// Pure function -PI isn't an external value
+const circleAreaPure = (radius, pi) => radius * radius * pi
+
+console.log(circleAreaPure(10, 3.14))
