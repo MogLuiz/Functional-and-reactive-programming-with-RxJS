b1af35be82248c560647f0c5af6c775c2092a434
Luiz Henrique <luizhjramos@outlook.com>
2022-01-15 17:12:03 -0300

    feat: solving algorithm more declaratively with reduce

--- functional/immutability_2.js
+++ functional/immutability_2.js
@@ -1,9 +1,16 @@
 const numbers = [10, 8, 12, 4, 1, 3, 5]
 
-let total = 0
+// #1 - Dados Mutáveis
+// let total = 0
 
-for(let i = 0; i < numbers.length; i++) {
-    total += numbers[i]
-}
+// for(let i = 0; i < numbers.length; i++) {
+//     total += numbers[i]
+// }
 
+// console.log(total)
+
+
+// #2 - Reduce
+const sum = (a, b) => a + b
+const total = numbers.reduce(sum)
 console.log(total)
