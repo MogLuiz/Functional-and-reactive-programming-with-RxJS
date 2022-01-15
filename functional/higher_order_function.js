803eeb525a5ae5b17c384aee0ccdd98b624d818f
Luiz Henrique <luizhjramos@outlook.com>
2022-01-14 23:53:22 -0300

    feat: passing flow from a higher order function

--- functional/higher_order_function.js
+++ functional/higher_order_function.js
@@ -1,1 +1,11 @@
 const run = (fn, ...params) => fn(...params)
+
+
+const sum = (a, b, c) => a + b + c
+
+
+const mult = (a, b) => a * b 
+
+
+console.log(run(sum, 4, 5, 6))
+console.log(run(mult, 4, 5))
