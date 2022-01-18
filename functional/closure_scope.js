151228c9b8451c134cd502aa173597a764e9ef7d
Luiz Henrique <luizhjramos@outlook.com>
2022-01-18 20:55:00 -0300

    feat: making the scope of the function deeper

--- functional/closure_scope.js
+++ functional/closure_scope.js
@@ -1,5 +1,12 @@
 const x = 3
 
-const sumXMore3 = () => x + 3
 
-module.exports = sumXMore3
+const outside = () => {
+    const x = 10
+    const sumXMore3 = () => x + 3
+
+    return sumXMore3
+}
+
+
+module.exports = outside()
