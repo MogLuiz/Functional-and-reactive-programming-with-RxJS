6a5f596f591d36579f53fb523980c92cdff53839
Luiz Henrique <luizhjramos@outlook.com>
2022-01-18 23:20:45 -0300

    refact: refactoring currying file

--- functional/currying_1_v3.js
+++ functional/currying_1_v3.js
@@ -1,4 +1,4 @@
-// Currying with arrow function
+
 const textWithSizeBetween = (min) => (max) => (error) => (text) => {
     const size = (text || "").trim().length
     
@@ -9,20 +9,6 @@ const textWithSizeBetween = (min) => (max) => (error) => (text) => {
     console.log(text)
 }
 
-// Currying with declarative function
-function textWithSizeBetween2(min) {
-    return function(max) {
-        return function(error) {
-            return function(text) {
-                const size = (text || "").trim().length
-
-                if(size < min || size > max) {
-                    throw error
-                }
-            }
-        }
-    }
-}
 
 const applyValidation = (fn) => (value) => {
     try {
@@ -38,7 +24,5 @@ const validNameProduct = applyValidation(forceValidNameProduct)
 
 
 const p1 = { name: "A", price: 14.99, desc: 0.25 }
-// textWithSizeBetween(4)(244)("Invalid Name!")(p1.name)
-// forceStandardSize("Invalid Name!")(p1.name)
-// forceValidNameProduct(p1.name)
+
 console.log(validNameProduct(p1.name))
