333ba005777581ce5c0ab56128a9bc8da4c52f25
Luiz Henrique <luizhjramos@outlook.com>
2022-01-18 23:07:21 -0300

    feat: apply validation with lazy evaluation

---
+++ functional/currying_1_v3.js
@@ -0,0 +1,44 @@
+// Currying with arrow function
+const textWithSizeBetween = (min) => (max) => (error) => (text) => {
+    const size = (text || "").trim().length
+    
+    if(size < min || size > max) {
+        throw error
+    }
+
+    console.log(text)
+}
+
+// Currying with declarative function
+function textWithSizeBetween2(min) {
+    return function(max) {
+        return function(error) {
+            return function(text) {
+                const size = (text || "").trim().length
+
+                if(size < min || size > max) {
+                    throw error
+                }
+            }
+        }
+    }
+}
+
+const applyValidation = (fn) => (value) => {
+    try {
+        fn(value)
+    } catch(e) {
+        return { error: e }
+    }
+}
+    
+const forceStandardSize = textWithSizeBetween(4)(255)
+const forceValidNameProduct = forceStandardSize("Invalid Name!")
+const validNameProduct = applyValidation(forceValidNameProduct)
+
+
+const p1 = { name: "A", price: 14.99, desc: 0.25 }
+// textWithSizeBetween(4)(244)("Invalid Name!")(p1.name)
+// forceStandardSize("Invalid Name!")(p1.name)
+// forceValidNameProduct(p1.name)
+console.log(validNameProduct(p1.name))
