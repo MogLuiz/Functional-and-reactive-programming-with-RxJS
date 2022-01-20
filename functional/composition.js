da876a9ac09b2fdfb8981db1668511ef2ab93090
Luiz Henrique <luizhjramos@outlook.com>
2022-01-19 23:06:51 -0300

    feat: composing the should and emphasize functions

--- functional/composition.js
+++ functional/composition.js
@@ -1,5 +1,16 @@
 const composer = (...fns) => (value) => {
-    return fns.reduce((fn, acc) => {
+    return fns.reduce((acc, fn) => {
         return fn(acc)
     }, value)
 }
+
+const shout = (text) => text.toUpperCase()
+
+const emphasize = (text) => `${text}!!!!`
+
+const result = composer(
+    shout,
+    emphasize
+)("Para")
+
+console.log(result)
