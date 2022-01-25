3a27f2603681df3c534c4656bf61afbfb7db2262
Luiz Henrique <luizhjramos@outlook.com>
2022-01-25 18:55:13 -0300

    feat: creating intermediate points in the composition of functions

--- functional/composition.js
+++ functional/composition.js
@@ -24,9 +24,18 @@ const exaggerated = composer(
     slowDown
 )
 
+const aLittleLessExaggerated = composer(
+    shout,
+    emphasize
+)
+
 const result2 = exaggerated("Para")
+const result22 = aLittleLessExaggerated("Para")
 const result3 = exaggerated("Cuidado piso molhado!")
+const result33 = aLittleLessExaggerated("Cuidado piso molhado!")
 
 console.log(result)
 console.log(result2)
+console.log(result22)
 console.log(result3)
+console.log(result33)
