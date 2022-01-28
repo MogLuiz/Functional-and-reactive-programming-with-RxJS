ffa8d45832071020ec4b3a1b9c94eddf38b89e2f
Luiz Henrique <luizhjramos@outlook.com>
2022-01-28 20:43:21 -0300

    feat: start and stop function

--- rxjs/data_stream.js
+++ rxjs/data_stream.js
@@ -1,9 +1,22 @@
-const numbersGenerate = (fn) => {
-    let num = 0
-    setInterval(() => {
-        fn(num++)
-    }, 1000)
+const numbersGenerate = () => {
+    return {
+        start(fn, interval = 1000) {
+            let num = 0
+            const i = setInterval(() => {
+                fn(num++)
+            }, interval)
+
+            return {
+                stop() {
+                    clearInterval(i)
+                }
+            }
+        }
+    }
 }
 
-numbersGenerate(number => console.log(`#1: ${number * 2}`))
-numbersGenerate(number => console.log(`$2: ${number + 100}`))
+const timer1 = numbersGenerate()
+timer1.start(number => console.log(`#1: ${number * 2}`), 1000)
+
+const timer2 = numbersGenerate()
+timer2.start(number => console.log(`$2: ${number + 100}`), 2000)
