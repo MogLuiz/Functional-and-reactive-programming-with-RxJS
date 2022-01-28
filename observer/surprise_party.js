be7fc793cff0fdbf24ef166dc9198b1209f5f166
Luiz Henrique <luizhjramos@outlook.com>
2022-01-27 21:00:08 -0300

    feat: readline instance

---
+++ observer/surprise_party.js
@@ -0,0 +1,8 @@
+const readline = require("readline")
+
+const getResponse = (question) => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+}
