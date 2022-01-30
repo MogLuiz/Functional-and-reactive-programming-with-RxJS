648a9f04359971530e6f4f404fb18c43ef4ed6bc
Luiz Henrique <luizhjramos@outlook.com>
2022-01-30 15:50:11 -0300

    feat: generating data stream from an array with from rxjs

--- rxjs/data_stream_3.js
+++ rxjs/data_stream_3.js
@@ -1,4 +1,4 @@
-const { interval } = require("rxjs")
+const { interval, from } = require("rxjs")
 
 const generateNumber = interval(500)
 
@@ -8,3 +8,5 @@ const sub2 = generateNumber.subscribe(console.log)
 
 setTimeout(() => sub1.unsubscribe(), 8000)
 setTimeout(() => sub2.unsubscribe(), 6000)
+
+from([1, 3, 5, 7, 8]).subscribe(console.log)
