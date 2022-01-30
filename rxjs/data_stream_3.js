5009f801fb0a2dd9f2acfe1a78a3c28dc1f5732e
Luiz Henrique <luizhjramos@outlook.com>
2022-01-30 15:40:51 -0300

    feat: cancel the observable subscription

--- rxjs/data_stream_3.js
+++ rxjs/data_stream_3.js
@@ -3,4 +3,6 @@ const { interval } = require("rxjs")
 const generateNumber = interval(500)
 
 
-generateNumber.subscribe(num => console.log(Math.pow(2, num)))
+const sub1 = generateNumber.subscribe(num => console.log(Math.pow(2, num)))
+
+sub1.unsubscribe()
